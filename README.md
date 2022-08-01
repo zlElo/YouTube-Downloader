6ef9c3ddb01c3a7bda62d2f8fc8b507de3c59f97
zlElo <108819252+zlElo@users.noreply.github.com>
2022-08-01 10:20:06 +0200

    Add files
    I add some files and the first version of the YT-Downloader

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # YouTube-Downloader
-A gui based YouTube-Downloader
+YouTube Downloader is a terminal based YouTube Video downloade!
+You can download the videos in the highest resolution. 
+
+Info: You can download the .py or the .exe file in the repository.
+
+Let's download YouTube Videos!
+
---
+++ YouTube-Downloader/yt-downloader.py
@@ -0,0 +1,36 @@
+from pytube import YouTube
+from pytube.cli import on_progress 
+
+print('YT-Downloader')
+print('')
+print('---------------------------------------------------------------------------------')
+print('You can Download every YT-Video what you want in the highest resoultion!')
+print('Paste the YouTube Link with STRG + V in this window and you download will starts.')
+print('You can see how much time is left with the progress bar')
+print('---------------------------------------------------------------------------------')
+print('')
+
+items = list(range(0, 57))
+l = len(items)
+
+#link question
+linkquestion = input("Enter the link:  ")
+print('')
+yt = YouTube(linkquestion, on_progress_callback=on_progress)
+
+# A list with all items
+items = list(range(0, 57))
+l = len(items)
+
+#details
+print('---------------------------------------------------------------------------------')
+print("Title: ",yt.title)
+print("Creator: ",yt.author)
+print("Views: ",yt.views)
+print('---------------------------------------------------------------------------------')
+
+#Getting highest res.
+ys = yt.streams.get_highest_resolution()
+
+#download video
+ys.download()
